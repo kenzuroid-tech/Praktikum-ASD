1620f2c93a1b6a30f7053fdc738374683b72ae14
kenzuroid-tech <sitinikmatussholihahdivti@gmail.com>
2025-02-14 21:15:23 +0700

    kubus

--- jobsheet1/kubus.java
+++ jobsheet1/kubus.java
@@ -1,50 +1,52 @@
 import java.util.Scanner;
+
 public class kubus {
-        
-        public static double hitungVolume(double s) {
-            return s*s*s;
-        }
+    public static double hitungVolume(double s) {
+        return s * s * s;
+    }
 
-        public static double hitungLuasPermukaan(double s) {
-            return 6*s*s;
-        }
+    public static double hitungLuasPermukaan(double s) {
+        return 6 * s * s;
+    }
 
-        public static double hitungKeliling(double s) {
-            return 12*s;
-        }
+    public static double hitungKeliling(double s) {
+        return 12 * s;
+    }
 
-        public static void tampilkanMenu(){
-            System.out.println("Pilih menu yang anda inginkan");
-            System.out.println("1. Hitung Volume Kubus");
-            System.out.println("2. Hitung Luas Permukaan Kubus");
-            System.out.println("3. Hitung Keliling Kubus");
-        }
+    public static void tampilkanMenu() {
+        System.out.println("\n1. Hitung Volume Kubus");
+        System.out.println("2. Hitung Luas Permukaan Kubus");
+        System.out.println("3. Hitung Keliling Kubus");
+        System.out.println("4. Keluar");
+        System.out.print("Pilih menu yang anda inginkan: ");
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan sisi kubus: ");
+        double sisi = sc.nextDouble();
 
-            System.out.println("Masukkan sisi kubus: ");
-            double sisi = sc.nextDouble();
-
+        while (true) {  
             tampilkanMenu();
             int pilihan = sc.nextInt();
 
             switch (pilihan) {
                 case 1:
-                    System.out.println("Volume kubus: " + hitungVolume(sisi));
+                    System.out.println("\nVolume kubus: " + hitungVolume(sisi));
                     break;
                 case 2:
-                    System.out.println("Luas permukaan kubus: " + hitungLuasPermukaan(sisi));
+                    System.out.println("\nLuas permukaan kubus: " + hitungLuasPermukaan(sisi));
                     break;
                 case 3:
-                    System.out.println("Keliling kubus: " + hitungKeliling(sisi));
+                    System.out.println("\nKeliling kubus: " + hitungKeliling(sisi));
                     break;
+                case 4:
+                    System.out.println("\nKeluar dari program.");
+                    return; 
                 default:
-                System.out.println("Pilihan tidak valid");
-                    break;
+                    System.out.println("\nPilihan tidak valid, coba lagi.");
             }
         }
-    
+    }
 }
-
