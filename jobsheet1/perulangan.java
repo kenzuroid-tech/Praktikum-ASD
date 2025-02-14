ebcdceae94c8302b03e8715dfc9e68bffb4d4f44
kenzuroid-tech <sitinikmatussholihahdivti@gmail.com>
2025-02-14 15:10:49 +0700

    perulangan

---
+++ jobsheet1/perulangan.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class perulangan {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan NIM : ");
+        long nim = input.nextLong();
+
+        int n = (int)(nim % 100);
+
+        if (n < 10) {
+            n += 10;
+        }
+        
+        System.out.print("n: ");
+        for (int i = 1; i <= n; i++) {
+            if (i == 6 || i == 10) {
+                continue;
+            }
+            
+            if (i % 2 == 1) {
+                System.out.print("* ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
