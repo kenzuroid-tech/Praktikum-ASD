9ba85aad4185d1d09521790aa1368c935730d5ad
kenzuroid-tech <sitinikmatussholihahdivti@gmail.com>
2025-03-01 14:51:19 +0800

    perubahan

--- jobsheet3/Mahasiswa22.java
+++ jobsheet3/Mahasiswa22.java
@@ -3,5 +3,12 @@ public class Mahasiswa22 {
     public String nama;
     public String kelas;
     public float ipk;   
-     
+
+    public void cetakInfo() {
+        System.out.println("NIM   : " + this.nim);
+        System.out.println("Nama  : " + this.nama);
+        System.out.println("Kelas : " + this.kelas);
+        System.out.println("IPK   : " + this.ipk);
+        System.out.println("-----------------------------");
+    }
 }
