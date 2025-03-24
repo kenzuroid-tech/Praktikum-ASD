e9e018686c043cbcc00441d6ae54b7eba44de06b
kenzuroid-tech <sitinikmatussholihahdivti@gmail.com>
2025-03-24 20:55:59 +0800

    perubahan di insertion ascending menjadi descending

--- jobsheet6/MahasiswaBerprestasi22.java
+++ jobsheet6/MahasiswaBerprestasi22.java
@@ -48,7 +48,7 @@ public class MahasiswaBerprestasi22 {
         for (int i = 1; i < listMhs.length; i++) {
             Mahasiswa22 temp = listMhs[i];
             int j = i;
-            while (j>0 && listMhs[j-1].ipk>temp.ipk) {
+            while (j>0 && listMhs[j-1].ipk<temp.ipk) {
                 listMhs[j]=listMhs[j-1];
                 j--;
             }
